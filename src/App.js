704e6d28a7eb199a903ec4414b4c72becbe213b0
Thomas Conger <t.e.conger@gmail.com>
2022-12-13 12:27:55 -0800

    refactored app.js to be a functional component from a class component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Clock, { ClockToggle } from './components/Clock';
 import Folder from './components/Folder';
-import Weather from './components/Weather';
+// import Weather from './components/Weather';
 import AutoComplete from './components/Auto';
 
 const names = [
@@ -21,27 +21,52 @@ const folders = [
   { title: 'three', content: 'Third folder here' }
 ];
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showClock: true
-    };
-  }
-  
-  toggleClock = () => this.setState({ showClock: !this.state.showClock });
-  
-  render () {
-    return (
-      <div className="widgets">
-        <Folder folders={folders} />
-        <Weather />
-        <ClockToggle toggleClock={this.toggleClock} />
-        {this.state.showClock && <Clock />}
-        <AutoComplete names={names} />
-      </div>
-    );
-  }
+// REFACTORED APP
+
+//   toggleClock = () => this.setState({ showClock: !this.state.showClock });
+
+function App (props) {
+
+  const [showClock, setShowClock] = useState(true)
+  const toggleClock = useEffect(()=>{
+    setShowClock(!showClock)
+  }, [])
+
+  return (
+          <div className="widgets">
+            <Folder folders={folders} />
+            {/* <Weather /> */}
+            <ClockToggle toggleClock={toggleClock} />
+            {showClock && <Clock />}
+            <AutoComplete names={names} />
+          </div>
+        )
 }
+
+
+// OLD APP COMPONENT
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       showClock: true
+//     };
+//   }
+
+//   toggleClock = () => this.setState({ showClock: !this.state.showClock });
+
+//   render () {
+//     return (
+//       <div className="widgets">
+//         <Folder folders={folders} />
+//         <Weather />
+//         <ClockToggle toggleClock={this.toggleClock} />
+//         {this.state.showClock && <Clock />}
+//         <AutoComplete names={names} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
